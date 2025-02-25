00016dce01d8eabc2076d4f1291b89623952c19a
MimAtia <mim15-5375@diu.edu.bd>
2025-02-25 12:37:23 +0600

    nevigation

---
+++ flutter_application_1/lib/body.dart/new.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppBody extends StatefulWidget {
+  @override
+  _AppBodyState createState() => _AppBodyState();
+}
+
+class _AppBodyState extends State<AppBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.power_off_outlined), label: "Power"),
+      ]), // BottomNavigationBar
+    ); // Scaffold
+  }
+}
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,61 +1,35 @@
 import 'package:flutter/material.dart';
-void main() {
-  runApp(const Lab06());
+
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyWidgetState();
 }
 
-class Lab06 extends StatelessWidget {
-  const Lab06({super.key});
+class _MyWidgetState extends State<Myapp> {
+  int currentIndex = 1;
+  final List<Widget> pages = [
+    Text("Home"),
+    Text("Profile"),
+  ];
+  void onClickMenu(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Advanced UI',
-      home: Scaffold(
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(
-                      Icons.access_alarm_outlined,
-                      size: 60,
-                      color: Colors.blue,
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Icon(Icons.closed_caption_rounded),
-                    ),
-                  ],
-                  ),
-                  ),
-              ListTile(
-                leading: Icon(Icons.people_alt_rounded),
-                title: Text("Contacts"),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text("Contacts"),
-                onTap: () {},
-              ),
-            ],
-          )),
-          appBar: AppBar(
-            title: Text("Advance UI"),
-          ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.network(
-                    "https://images.unsplash.com/photo-1635805737707-575885ab0820?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-                Image.network(
-                    "https://images.unsplash.com/photo-1635805739892-ab7b431400f7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-              ],
-            ),
-          ),
-      ),
+    return Scaffold(
+      body:pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onClickMenu,
+        currentIndex: currentIndex,
+        items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+      ]),
     );
   }
 }
